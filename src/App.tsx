350eb76cffd8c807557671d1b9f3b505d6e155c5
SanjayRyuzaki <106882956+SanjayRyuzaki@users.noreply.github.com>
2025-03-14 10:15:00 +0530

    feat: add scroll-to-top button with smooth animations

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Index from './pages/Index';
 import PerformanceMonitor from './components/PerformanceMonitor';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
       <Index />
       <PerformanceMonitor />
+      <ScrollToTop />
     </div>
   );
 }
